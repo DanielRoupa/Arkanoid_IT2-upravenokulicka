443011427563380e6d44ae728999dc923199690d
Daniel Roupa <roupadaniel058@gmail.com>
2024-05-16 10:58:23 +0200

    Přidejte soubory projektu.

---
+++ Arkanoid_IT2/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Arkanoid_IT2
+{
+    public class Ball : GameObject
+    {
+        public int Size { get; }
+
+        public Ball(int size)
+        {
+            Size = size;
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = 30;
+            ellipse.Height = 30;
+            ellipse.Fill = Brushes.Red;
+            Canvas.SetBottom(ellipse, 20);
+            Canvas.SetLeft(ellipse, Location.X - Size / 2);
+            canvas.Children.Add(ellipse);
+
+        }
+    }
+}
---
+++ Arkanoid_IT2/Board.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace Arkanoid_IT2
+{
+    public class Board : GameObject // : znamená že třída dědí ínformace od jiné třídy
+    {
+        public int Size { get; }
+
+        public Board(int size) 
+        {
+            Size = size; 
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = Size;
+            rectangle.Height = 15;
+            rectangle.Fill = Brushes.White;
+            Canvas.SetBottom(rectangle, 20);
+            Canvas.SetLeft(rectangle, Location.X - Size/2);
+            canvas.Children.Add(rectangle);
+
+        }
+    }
+}
